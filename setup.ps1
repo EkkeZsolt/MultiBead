d4be948fdfc8b545d889ef9367b380d20c3435d0
EkkeZsolt <ekkezsolt@gmail.com>
2025-11-23 19:37:50 +0100

    fix:req.txt

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,6 @@
 py -3.13 -m venv venv
 .\venv\Scripts\Activate.ps1
 pip install --upgrade pip
-pip install -r run/requirements.txt
+pip install -r requirements.txt
 Start-Process -NoNewWindow -FilePath "uvicorn" -ArgumentList "run.backend:app --reload"
 streamlit run run/frontend.py
--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 python3.13 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install -r run/requirements.txt
+pip install -r requirements.txt
 nohup uvicorn run.backend:app --reload > backend.log 2>&1 &
 streamlit run run/frontend.py
